483a3197f1db104a83ce7ab70dbdee0e05127efb
xeusteerapat <xeus085@gmail.com>
2020-05-13 11:44:22 +0700

    change endpoint routes

--- server.js
+++ server.js
@@ -7,12 +7,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Add new player to database with POST method
-app.post('/add-player', (req, res) => {
+app.post('/players', (req, res) => {
   let newPlayer = {
     name: req.body.name,
     position: req.body.position,
     year: Number(req.body.year)
   };
+
   db.player
     .create(newPlayer)
     .then(result => res.status(200).send(result))
@@ -24,15 +25,23 @@ app.get('/players', (req, res) => {
   db.player.findAll().then(result => res.status(200).send(result));
 });
 
+// Retrived one player
+app.get('/player/:id', (req, res) => {
+  const { id } = req.body;
+  db.player
+    .findOne({ where: { id } })
+    .then(result => res.status(200).send(result));
+});
+
 // Edit player name
-app.put('/edit-player/:id', (req, res) => {
+app.put('/players/:id', (req, res) => {
   db.player
     .update({ name: req.body.name }, { where: { id: Number(req.params.id) } })
     .then(() => res.status(200).send(req.body));
 });
 
 // Delete player from id
-app.delete('/delete-player/:id', (req, res) => {
+app.delete('/players/:id', (req, res) => {
   db.player
     .destroy({ where: { id: Number(req.params.id) } })
     .then(() => res.send('Player deleted!'));
